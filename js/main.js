9864780421148422c966ab9485c5c1f037850572
Marlon <luciousmc@icloud.com>
2020-05-24 23:50:46 -0700

    adds smooth scrolling to links

--- js/main.js
+++ js/main.js
@@ -15,5 +15,26 @@ navBar.addEventListener('click', function(event) {
       }
     }
   }
+});
 
+// SMOOTH SCROLLING
+function getScrollPos(element) {
+  let xPos = 0, yPos = 0;
+
+  xPos += (element.offsetLeft - element.scrollLeft + element.clientLeft);
+  yPos += (element.offsetTop - element.scrollTop + element.clientTop);
+
+  return {
+    top: yPos,
+    left: xPos
+  };
+}
+
+menu.addEventListener('click', function(event) {
+  event.preventDefault();
+  const hash = event.target.hash;
+  const scrollTarget = document.querySelector(hash);
+  const { top } = getScrollPos(scrollTarget);
+
+  window.scroll({ top: top - navBar.offsetHeight, left: 0, behavior: 'smooth'})
 });
