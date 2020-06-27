5954b7ef92917d9c566084a563708a0be4b970c5
Marlon <luciousmc@icloud.com>
2020-06-27 15:13:23 -0700

    replaces menu icon with font awesome bars

--- js/main.js
+++ js/main.js
@@ -1,40 +1,55 @@
 // SHOW HIDE MENU ON MOBILE
-const navBar = document.querySelector('.nav-container');
-const menu = document.getElementById('menu');
+const navBar = document.querySelector(".nav-container");
+const menu = document.getElementById("menu");
 
-navBar.addEventListener('click', function(event) {
-
-  if (event.target.attributes.class){
-    const { value } = event.target.attributes.class;
-
-    if (value === 'nav-toggle') {
-      if (menu.classList.contains('show-menu')) {
-        menu.classList.remove('show-menu');
-      } else {
-        menu.classList.add('show-menu');
-      }
+navBar.addEventListener("click", function (event) {
+  console.log(event);
+  if (event.target.classList.contains("nav-toggle")) {
+    if (menu.classList.contains("show-menu")) {
+      menu.classList.remove("show-menu");
+    } else {
+      menu.classList.add("show-menu");
     }
   }
+
+  // if (event.target.attributes.class) {
+  //   const { value } = event.target.attributes.class;
+
+  //   console.log(event);
+
+  //   if (value === "nav-toggle") {
+  //     if (menu.classList.contains("show-menu")) {
+  //       menu.classList.remove("show-menu");
+  //     } else {
+  //       menu.classList.add("show-menu");
+  //     }
+  //   }
+  // }
 });
 
 // SMOOTH SCROLLING
 function getScrollPos(element) {
-  let xPos = 0, yPos = 0;
+  let xPos = 0,
+    yPos = 0;
 
-  xPos += (element.offsetLeft - element.scrollLeft + element.clientLeft);
-  yPos += (element.offsetTop - element.scrollTop + element.clientTop);
+  xPos += element.offsetLeft - element.scrollLeft + element.clientLeft;
+  yPos += element.offsetTop - element.scrollTop + element.clientTop;
 
   return {
     top: yPos,
-    left: xPos
+    left: xPos,
   };
 }
 
-menu.addEventListener('click', function(event) {
+menu.addEventListener("click", function (event) {
   event.preventDefault();
   const hash = event.target.hash;
   const scrollTarget = document.querySelector(hash);
   const { top } = getScrollPos(scrollTarget);
 
-  window.scroll({ top: top - navBar.offsetHeight, left: 0, behavior: 'smooth'})
+  window.scroll({
+    top: top - navBar.offsetHeight,
+    left: 0,
+    behavior: "smooth",
+  });
 });
